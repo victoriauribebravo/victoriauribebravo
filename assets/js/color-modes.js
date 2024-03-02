94011ac08ed80ce6cbfc4159ca77cd6b641307c7
adedios <adedios@idealista.com>
2024-03-02 22:22:40 +0100

    changed icons and added responsive image

--- assets/js/color-modes.js
+++ assets/js/color-modes.js
@@ -20,11 +20,7 @@
   }
 
   const setTheme = theme => {
-    if (theme === 'auto') {
-      document.documentElement.setAttribute('data-bs-theme', (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light'))
-    } else {
-      document.documentElement.setAttribute('data-bs-theme', theme)
-    }
+      document.documentElement.setAttribute('data-bs-theme', 'light')
   }
 
   setTheme(getPreferredTheme())
